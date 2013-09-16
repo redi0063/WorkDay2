2469a8af57195015402264604911860ef7ffc2d2
idsc3101 <idsc3101@gmail.com>
2013-09-16 17:24:46 -0500

    Named Firstname Workday 2.

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,33 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullname As String
+
+        'Read the input from the user'
+        fullname = txtFullName.Text
+
+        'Trimmed Full Name'
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        'Display it back to user'
+        txtFirstName.Text = fullname
+
+        'Find the index between first space'
+        Dim idxSpace As Integer
+        idxSpace = fullnameTrimmed.IndexOf(" ")
+
+        'Extract the first name'
+        Dim firstName As String
+        firstName = fullnameTrimmed.Substring(0, idxSpace)
+
+        'Display First name to user'
+        txtFirstName.Text = firstName
+
+    End Sub
+
+
+    Private Sub txtFullName_TextChanged(sender As System.Object, e As System.EventArgs) Handles txtFullName.TextChanged
 
     End Sub
 End Class
